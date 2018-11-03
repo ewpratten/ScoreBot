cd2d7ae7a4f6ef2f6ac923e3dbb786386da6a6ff
Evan Pratten <ewpratten@gmail.com>
2018-11-03 15:33:16 -0400

    Update __main__.py

--- ScoreBot/__main__.py
+++ ScoreBot/__main__.py
@@ -11,5 +11,5 @@ print(config)
 all_matches = tba.getEventData(config["tba_key"], config["tba_endpoint"], config["event_key"], config["team_number"])
 newest_match = all_matches[0]
 parsed_match = tba.parseMatch(config["team_number"], newest_match)
-slack.formatSend(config["slack_webhook_url"], parsed_match)
+slack.formatSend(config["slack_webhook_url"], parsed_match, config["twitter_username"])
 
